1355b32a74acf55d40ddf7826478d274b1f84ae6
trojandeveloper <happysmile7094@gmail.com>
2023-03-26 05:01:38 -0400

    Updates

---
+++ precache-manifest.6219012c6aa3826b24aa849191524629.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "add19cfaeca21213142f",
+    "url": "/static/css/main.957fbca4.chunk.css"
+  },
+  {
+    "revision": "add19cfaeca21213142f",
+    "url": "/static/js/main.add19cfa.chunk.js"
+  },
+  {
+    "revision": "2c847ea04985c4926cc1",
+    "url": "/static/css/1.f67deb79.chunk.css"
+  },
+  {
+    "revision": "2c847ea04985c4926cc1",
+    "url": "/static/js/1.2c847ea0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d7ae5ac6e934401e57e82f70b996bc1",
+    "url": "/static/media/ab-img.8d7ae5ac.png"
+  },
+  {
+    "revision": "3678a3d46205ba8ca1bcf9a59fb86de2",
+    "url": "/static/media/profile.3678a3d4.jpg"
+  },
+  {
+    "revision": "2f03d3386ddfaa9ce57f5f23c89927d7",
+    "url": "/index.html"
+  }
+];
